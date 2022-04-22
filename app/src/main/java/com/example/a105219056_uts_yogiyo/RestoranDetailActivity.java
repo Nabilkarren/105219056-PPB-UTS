edb500d4a13b38341bb1abd70400aa41201697b7
Nabil <82162563+Nabilkarren@users.noreply.github.com>
2022-04-22 23:01:33 +0700

    Commit UTS - 2

--- app/src/main/java/com/example/a105219056_uts_yogiyo/RestoranDetailActivity.java
+++ app/src/main/java/com/example/a105219056_uts_yogiyo/RestoranDetailActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class RestoranDetailActivity extends AppCompatActivity {
+public class RestoranDetailActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageButton backButton;
+    private Button shareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,17 @@ public class RestoranDetailActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        shareButton = findViewById(R.id.btn_share);
+        shareButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btn_share:
+                Toast.makeText(getApplicationContext(),"Shared", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
